e5469312d9fae3728841c11b566ee5624848e029
YO <y.otsubo.dss@gmail.com>
2018-10-17 01:15:10 +0900

    殴るぞ追加

--- src/naguruReply.js
+++ src/naguruReply.js
@@ -1,12 +1,12 @@
-//CHANNEL_ACCESS_TOKENを設定
-//LINE developerで登録をした、自分のCHANNEL_ACCESS_TOKENを入れて下さい
+import axios from 'axios'
 
-var line_endpoint = 'https://api.line.me/v2/bot/message/reply';
+exports.handler = async function(event, context, callback) {
+  const body = JSON.parse(event.body)
+  const targetEvent = body.events[0]
 
 //ポストで送られてくるので、ポストデータ取得
 //JSONをパースする
-function doPost(e) {
-  var json = JSON.parse(e.postData.contents);
+  var json = JSON.parse(event.postData.contents);
 
   //返信するためのトークン取得
   var reply_token= json.events[0].replyToken;
@@ -45,7 +45,65 @@ function doPost(e) {
   var messages = reply_messages.map(function (v) {
     return {'type': 'text', 'text': v};
   });
-  UrlFetchApp.fetch(line_endpoint, {
+
+
+
+ /* const data = {
+    replyToken: targetEvent.replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: '今日何食べたい？',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '寿司',
+                text: '寿司'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '焼肉',
+                text: '焼肉'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'とんかつ',
+                text: 'とんかつ'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: 'ラーメン',
+                text: 'ラーメン'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                label: '日本酒',
+                text: '日本酒'
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }*/
+
+/*
+UrlFetchApp.fetch(line_endpoint, {
     'headers': {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
@@ -57,4 +115,21 @@ function doPost(e) {
     }),
   });
   return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+  */
+
+
+
+  await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'replyToken': reply_token,
+      'messages': messages,
+    }),
+  })
+
+  callback(null, {})
 }
