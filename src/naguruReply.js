cd74a1947d89ba062e7c9e607d66dcabcb515573
YO <y.otsubo.dss@gmail.com>
2018-10-17 01:01:48 +0900

    殴るぞ追加

--- src/naguruReply.js
+++ src/naguruReply.js
@@ -1,69 +1,59 @@
-import axios from 'axios'
+//CHANNEL_ACCESS_TOKENを設定
+//LINE developerで登録をした、自分のCHANNEL_ACCESS_TOKENを入れて下さい
 
-exports.handler = async function(event, context, callback) {
-  const body = JSON.parse(event.body)
-  const targetEvent = body.events[0]
+var line_endpoint = 'https://api.line.me/v2/bot/message/reply';
 
-  const data = {
-    replyToken: targetEvent.replyToken,
-    messages: [
-      {
-        type: 'text',
-        text: '今日何食べたい？',
-        quickReply: {
-          items: [
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                label: '寿司',
-                text: '寿司'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                label: '焼肉',
-                text: '焼肉'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                label: 'とんかつ',
-                text: 'とんかつ'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                label: 'ラーメン',
-                text: 'ラーメン'
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: 'message',
-                label: '日本酒',
-                text: '日本酒'
-              }
-            }
-          ]
-        }
-      }
-    ]
+//ポストで送られてくるので、ポストデータ取得
+//JSONをパースする
+function doPost(e) {
+  var json = JSON.parse(e.postData.contents);
+
+  //返信するためのトークン取得
+  var reply_token= json.events[0].replyToken;
+  if (typeof reply_token === 'undefined') {
+    return;
   }
 
-  await axios.post('https://api.line.me/v2/bot/message/reply', data, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
-    }
-  })
+  //送られたLINEメッセージを取得
+  var user_message = json.events[0].message.text;
 
-  callback(null, {})
+  //返信する内容を作成
+  var reply_messages;
+  var random = Math.floor( Math.random() *10 );
+  if(random == 0) {
+    reply_messages = ['腹パンね',];
+  }
+ else if ('こんにちわ' == user_message) {
+    //かっこいいと入力された際
+    reply_messages = ['殴るぞ',];
+
+  } else if ('おはよう' == user_message) {
+    //かわいいと入力された際
+    reply_messages = ['てめぇふざけてんのか',];
+
+  } else if ('おやすみ' == user_message) {
+    //普通と入力された際
+    reply_messages = ['おやすみ',];
+
+  } else {
+    //かっこいい、かわいい、普通が入力されたときの処理
+    reply_messages = ['「こんにちわ」って挨拶しろ'];
+  }
+
+  // メッセージを返信
+  var messages = reply_messages.map(function (v) {
+    return {'type': 'text', 'text': v};
+  });
+  UrlFetchApp.fetch(line_endpoint, {
+    'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'replyToken': reply_token,
+      'messages': messages,
+    }),
+  });
+  return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
 }
